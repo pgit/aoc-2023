b4df7806c98040fb131a4d042a56aee7a3c8423e
Peter Eisenlohr <peter@eisenlohr.org>
2023-12-09 12:36:02 +0000

    cleanup, try rv::istream for parsing list of numbers

--- 9ab.cpp
+++ 9ab.cpp
@@ -1,58 +1,48 @@
 #include <cassert>
-#include <charconv>
 #include <fstream>
+#include <sstream>
 #include <string>
 using namespace std::literals;
 
 #include <range/v3/algorithm/all_of.hpp>
 #include <range/v3/range/conversion.hpp>
 #include <range/v3/view/drop.hpp>
-#include <range/v3/view/filter.hpp>
-#include <range/v3/view/iota.hpp>
-#include <range/v3/view/map.hpp>
-#include <range/v3/view/split.hpp>
+#include <range/v3/view/istream.hpp>
 #include <range/v3/view/transform.hpp>
 #include <range/v3/view/zip.hpp>
+#include "range/v3/view/istream.hpp"
 using namespace ranges;
 using namespace ranges::views;
 
-#define BOOST_REGEX_MATCH_EXTRA
-#include <boost/regex.hpp>
-
 #include <fmt/ostream.h>
 
-std::optional<std::string> getline(std::ifstream& file)
+struct Result
 {
-   std::string line;
-   if (std::getline(file, line))
-      return line;
-   else
-      return std::nullopt;
-}
-
-template <class T>
-const auto to_number = transform(
-   [](auto&& rng)
+   long front = {};
+   long back = {};
+   const Result& operator+=(const Result& other) noexcept
    {
-      T l{};
-      if (ranges::distance(rng))
-         std::from_chars(&*rng.begin(), &*rng.begin() + ranges::distance(rng), l);
-      return l;
-   });
+      front += other.front;
+      back += other.back;
+      return *this;
+   }
+};
 
-std::pair<long, long> delta(const std::vector<long>& numbers)
+Result delta(const std::vector<long>& numbers)
 {
    fmt::println("[{}]", fmt::join(numbers, " "));
 
    if (all_of(numbers, [&](auto n) { return n == 0; }))
-      return std::make_pair(0, 0);
+      return {};
 
    std::vector deltas = zip(numbers, numbers | drop(1)) |
                         transform([](auto pair) { return pair.second - pair.first; }) |
                         to<std::vector>;
 
    auto decend = delta(deltas);
-   return {decend.first + numbers[numbers.size() - 1], numbers[0] - decend.second};
+   auto result = Result{numbers[0] - decend.front, decend.back + numbers[numbers.size() - 1]};
+   fmt::println("[{} {} {}]", result.front, fmt::join(numbers, " "), result.back);
+   return result;
 }
 
 int main(int argc, char* argv[])
@@ -60,15 +50,15 @@ int main(int argc, char* argv[])
    assert(argc == 2);
    std::ifstream file(argv[1]);
 
-   long A = 0, B = 0;
-   while (auto line = getline(file))
+   Result result;
+   std::string line;
+   while (std::getline(file, line))
    {
-      auto numbers = *line | split(' ') | to_number<long> | to<std::vector>;
-      auto deltas = delta(numbers);
-      A += deltas.first;
-      B += deltas.second;
+      std::istringstream ss{line};
+      auto numbers = istream<long>(ss) | ranges::to<std::vector>;
+      result += delta(numbers);
       fmt::println("");
    }
-   
-   fmt::println("A: {} B: {}", A, B);
+
+   fmt::println("A: {} B: {}", result.back, result.front);
 }
