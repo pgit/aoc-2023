41fb76bc908883fd4554c8324416b1c51e72fb54
Peter Eisenlohr <peter@eisenlohr.org>
2023-12-10 17:05:05 +0000

    remove start symbol before doing scan line algorithm

--- src/10ab.cpp
+++ src/10ab.cpp
@@ -2,13 +2,13 @@
 // https://adventofcode.com/2023/day/10
 //
 #include <set>
-#include <sstream>
 #include <string>
 using namespace std::literals;
 
 #include <range/v3/algorithm/all_of.hpp>
 #include <range/v3/range/conversion.hpp>
 #include <range/v3/view/drop.hpp>
+#include <range/v3/view/iota.hpp>
 #include <range/v3/view/istream.hpp>
 #include <range/v3/view/transform.hpp>
 #include <range/v3/view/zip.hpp>
@@ -17,12 +17,12 @@ using namespace ranges::views;
 
 #include "common.hpp"
 
-struct Pos
+struct Coord
 {
    int x = -1, y = -1;
-   bool operator==(const Pos& r) const = default;
-   Pos operator+(const Pos& r) const { return Pos{x + r.x, y + r.y}; }
-   const Pos& operator+=(const Pos& r)
+   bool operator==(const Coord& r) const = default;
+   Coord operator+(const Coord& r) const { return Coord{x + r.x, y + r.y}; }
+   const Coord& operator+=(const Coord& r)
    {
       x += r.x;
       y += r.y;
@@ -33,7 +33,7 @@ struct Pos
 struct Pipe
 {
    char symbol;
-   std::vector<Pos> connections;
+   std::vector<Coord> connections;
    std::string unicodeSymbol;
    bool visited = false;
    auto operator<=>(const Pipe& other) const noexcept { return symbol <=> other.symbol; }
@@ -59,10 +59,11 @@ const std::set<Pipe, std::less<>> PIPES = {
    {'I', {},                     "\x1b[44m \x1b[0m"},
    {'O', {},                     "O"},
    {'S', {{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}}, "\x1b[1;31mS\x1b[0m"},
+   {'X', {{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}}, "\x1b[1;31mX\x1b[0m"},
    // clang-format on
 };
 
-const std::array<Pos, 4> AROUND = {{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}};
+const std::array<Coord, 4> AROUND = {{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}};
 
 const Pipe DOT = *PIPES.find('.');
 
@@ -73,31 +74,26 @@ struct Map
       for (std::string line; std::getline(file, line);)
          map.emplace_back(line | transform([](char c) { return *PIPES.find(c); }) |
                           to<std::vector>);
-      width = map[0].size();
-      height = map.size();
 
-      for (int y = 0; y < height; ++y)
-         for (int x = 0; x < height; ++x)
-            if (at({x, y}) == 'S')
-               start = Pos(x, y);
+      for (auto [row, y] : zip(map, ints))
+         for (auto [cell, x] : zip(row, ints))
+            if (cell.symbol == 'S')
+               start = Coord(x, y);
    }
 
    std::vector<std::vector<Pipe>> map;
-   Pos start;
-   int width;
-   int height;
+   Coord start;
 
-   Pipe& at(const Pos& c)
+   Pipe& at(const Coord& c)
    {
+      static Pipe mydot = DOT;
       if (c.y >= 0 && c.y < map.size())
          if (c.x >= 0 && c.x <= map[c.y].size())
             return map[c.y][c.x];
-
-      static Pipe mydot = DOT;
       return mydot; // yes I know I'm returning a non-const reference to a static here
    }
 
-   bool isConnected(const Pos& a, const Pos& b)
+   bool isConnected(const Coord& a, const Coord& b)
    {
       for (auto ca : at(a).connections)
          for (auto cb : at(b).connections)
@@ -124,8 +120,11 @@ int main(int argc, char* argv[])
 {
    Map map(input(argc, argv));
 
-   std::vector<Pos> track{};
+   assert(map.start.x != -1 && map.start.y != -1);
+   fmt::println("start at ({}, {})", map.start.x, map.start.y);
+
    auto pos = map.start;
+   std::vector<Coord> track{};
    for (;;)
    {
       bool dead_end = true;
@@ -143,9 +142,13 @@ int main(int argc, char* argv[])
             break;
          }
       }
-      if (pos == map.start)
+
+      if (dead_end || pos == map.start)
          break;
    };
+
+   map.dump();
+   fmt::println("A {} / 2 = {}", track.size(), track.size() / 2);
 
    //
    // Clear out any garbage, i.e. any pipe symbols that are not part of the loop
@@ -154,6 +157,16 @@ int main(int argc, char* argv[])
       for (auto& cell : row)
          if (!cell.visited)
             cell = DOT;
+
+   //
+   // Replace 'S' with correct symbol, so scan line algorithm works:
+   //
+   for (auto& pipe : PIPES)
+   {
+      map.at(map.start) = pipe;
+      if (map.isConnected(map.start, track[1]) && map.isConnected(map.start, track.back()))
+         break;
+   }
 
    //
    // Scan rows: Any encounter of '|', "┌┄┘" or "└┄┐" toggles the inside flag, and any encounter
@@ -190,16 +203,16 @@ int main(int argc, char* argv[])
             if (start.symbol == 'F' && it->symbol == 'J' || // ┌┄┘
                 start.symbol == 'L' && it->symbol == '7') // └┄┐
                inside = !inside;
-            break;
          }
+         break;
 
          default:
+            fmt::println("unexpected symbol '{}'", it->symbol);
             assert(false);
          }
       }
    }
 
    map.dump();
-   fmt::println("A {} / 2 = {}", track.size(), track.size() / 2);
    fmt::println("B {}", count);
 }
