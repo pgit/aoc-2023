c275a99a843d52e2baf6e18eb0973c844b8d68db
Peter Eisenlohr <peter@eisenlohr.org>
2023-12-11 09:09:30 +0000

    do both parts w/o recompiling

--- src/11ab.cpp
+++ src/11ab.cpp
@@ -4,6 +4,7 @@
 #include <set>
 
 #include <range/v3/algorithm/all_of.hpp>
+#include <range/v3/range/conversion.hpp>
 #include <range/v3/view/filter.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/istream.hpp>
@@ -21,29 +22,26 @@ struct Coord
    Coord operator+(const Coord& r) const { return Coord{x + r.x, y + r.y}; }
 };
 
-int main(int argc, char* argv[])
+int computeDistances(std::ifstream& file, int factor)
 {
-   auto file = input(argc, argv);
-
-   const int EXPANSION = 2; // A: 2 B: 1'000'000
+   assert(factor >= 1);
 
    //
    // parse input file, applying vertical expansion on the fly
    //
-   std::set<int> used_x;
    std::set<Coord> galaxies;
    {
+      file.clear();
+      file.seekg(0);
       std::string line;
       for (int y = 0; std::getline(file, line); y++)
       {
          if (all_of(line, [](char c) { return c == '.'; }))
-            y += EXPANSION - 1;
+            y += factor - 1;
          else
             for (auto galaxy : zip(line, iota(0)) | filter([](auto p) { return p.first == '#'; }) |
                                   transform(
-                                     [&](auto p)
-                                     {
-                                        used_x.insert(p.second);
+                                     [&](auto p) {
                                         return Coord{p.second, y};
                                      }))
                galaxies.insert(galaxy);
@@ -57,13 +55,14 @@ int main(int argc, char* argv[])
    //
    std::set<Coord> expanded;
    {
+      auto used_x = galaxies | transform(&Coord::x) | to<std::set>;
       auto it = galaxies.begin();
       int expansion = 0, last_x = *used_x.begin();
       for (int x : used_x)
       {
-         expansion += std::max(0, x - last_x - 1) * (EXPANSION - 1);
-         for (; it->x == x; ++it)
-            expanded.insert(*it + Coord{expansion, 0});
+         expansion += std::max(0, x - last_x - 1) * (factor - 1);
+         while (it->x == x)
+            expanded.insert(*it++ + Coord{expansion, 0});
          last_x = x;
       }
    }
@@ -75,5 +74,15 @@ int main(int argc, char* argv[])
    for (auto ia = expanded.begin(); ia != expanded.end(); ++ia)
       for (auto ib = ia; ib != expanded.end(); ++ib)
          sum += abs(ib->x - ia->x) + abs(ib->y - ia->y);
-   fmt::println("A/B: {} (factor {})", sum, EXPANSION);
+
+   return sum;
+}
+
+int main(int argc, char* argv[])
+{
+   auto file = input(argc, argv);
+   fmt::println("A: {}", computeDistances(file, 2));
+   fmt::println("B: {} (*10)", computeDistances(file, 10));
+   fmt::println("B: {} (*100)", computeDistances(file, 100));
+   fmt::println("B: {} (*1M)", computeDistances(file, 1'000'000));
 }
