cc7720156c2532ac27d7fc48671329e91635aeef
Peter Eisenlohr <peter@eisenlohr.org>
2023-12-15 21:35:55 +0000

    cleanup

--- src/14ab.cpp
+++ src/14ab.cpp
@@ -6,6 +6,7 @@
 #include <range/v3/algorithm/all_of.hpp>
 #include <range/v3/numeric/accumulate.hpp>
 #include <range/v3/range/conversion.hpp>
+#include <range/v3/view/for_each.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/join.hpp>
 #include <range/v3/view/transform.hpp>
@@ -72,6 +73,17 @@ struct Row
    size_t size() const { return w; }
    auto begin() const { return ColumnIterator{p0, dx}; }
    auto end() const { return ColumnIterator{p0 + dx * w, dx}; }
+
+   // reversed weight function, pass south view if you want the north weight
+   size_t weight()
+   {
+      return accumulate(zip(reverse(*this), iota(1)) |
+                           filter([](auto a) { return a.first == 'O'; }) |
+                           transform([](auto a) { return a.second; }),
+                        size_t{0});
+   }
+
+   void slide();
 };
 
 static_assert(std::ranges::range<Row>);
@@ -114,7 +126,7 @@ struct RowIterator
 static_assert(std::forward_iterator<RowIterator>);
 static_assert(!std::bidirectional_iterator<RowIterator>);
 
-struct View 
+struct View
 {
    char* p0;
    const ssize_t dy, dx; // delta within view to go to next row / column
@@ -124,7 +136,6 @@ struct View
    RowIterator begin() const { return {row(0), dy}; }
    RowIterator end() const { return {row(h), dy}; }
 };
-
 
 static_assert(std::ranges::range<View>);
 static_assert(ranges::range<View>);
@@ -182,25 +193,35 @@ struct Map
       return std::hash<std::string_view>{}(std::string_view(m_data.data(), m_data.size()));
    }
 
+   void slide(Direction dir)
+   {
+      for (auto row : view(dir))
+         row.slide();
+   }
+
+   size_t weight(Direction dir)
+   {
+      return accumulate(view(dir) | transform(&Row::weight), size_t{0});
+   }
+
+   void dump(Direction direction = Direction::west)
+   {
+      for (auto row : view(direction))
+         fmt::println("{}", fmt::join(row, ""));
+      fmt::println("");
+   }
+
    const size_t w, h, dy;
    std::vector<char> m_data;
 };
-
-// reversed weight function, pass south view if you want the north weight
-size_t weight(Row& row)
-{
-   return accumulate(zip(reverse(row), iota(1)) | filter([](auto a) { return a.first == 'O'; }) |
-                        transform([](auto a) { return a.second; }),
-                     size_t(0));
-}
 
 //
 // This implementation relies on a non-[.#O]-border around the map, avoiding a lot of checks
 // against vec.end().
 //
-void slide(Row& vec)
+void Row::slide()
 {
-   for (auto p0 = vec.begin(); p0 != vec.end();)
+   for (auto p0 = begin(); p0 != end();)
    {
       //
       //    OO.O.O..#.O
@@ -236,24 +257,6 @@ void slide(Row& vec)
    }
 }
 
-void slide(Map& map, Direction dir)
-{
-   for (auto row : map.view(dir))
-      slide(row);
-}
-
-size_t weight(Map& map, Direction dir)
-{
-   return accumulate(map.view(dir) | transform([](auto row) { return weight(row); }), 0UL);
-}
-
-void dump(Map& map, Direction direction = Direction::west)
-{
-   for (auto row : map.view(direction))
-      fmt::println("{}", fmt::join(row, ""));
-   fmt::println("");
-}
-
 int main(int argc, char* argv[])
 {
    auto file = input(argc, argv);
@@ -273,11 +276,11 @@ int main(int argc, char* argv[])
    //
    // part A: slide north once
    //
-   slide(map, Direction::north);
+   map.slide(Direction::north);
 
    // dump(map);
 
-   size_t A = weight(map, Direction::north);
+   size_t A = map.weight(Direction::north);
    fmt::println("A: {}", A);
 
    //
@@ -292,8 +295,7 @@ int main(int argc, char* argv[])
       // perform a cycle of sliding N, W, S, E
       //
       for (auto dir : {Direction::north, Direction::west, Direction::south, Direction::east})
-         for (auto row : map.view(dir))
-            slide(row);
+         map.slide(dir);
 
       //
       // detect loops by keeping a set of hashes
@@ -316,7 +318,7 @@ int main(int argc, char* argv[])
          assert((cycle - t0) % loop_size == 0); // sanity check, but usually not reached
    }
 
-   dump(map);
-   size_t B = weight(map, Direction::north);
+   map.dump();
+   size_t B = map.weight(Direction::north);
    fmt::println("A: {} B: {}", A, B);
 }
